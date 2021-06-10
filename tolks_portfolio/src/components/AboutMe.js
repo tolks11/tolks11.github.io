b0185f35494d1457d413dabe961c807565883a4b
Erik Toelke <tolks@Eriks-MacBook-Pro.local>
2021-06-10 14:22:53 -0400

    inital commit

--- tolks_portfolio/src/components/AboutMe.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutMe = () => {
-    return(
-        <h1>deeply ineterested in anything and everything crypto, specifically ethereuem & zk-technology</h1>
-    )
-}
-
-export default AboutMe
--- tolks_portfolio/src/components/Blog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Blog = () => {
-    return(
-        <h1>Things I've written recently: </h1>
-    )
-}
-
-export default Blog
